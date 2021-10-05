88ac54478803da1fb73a8c206704986e3366094f
Diogo Oliveira <88461069+diiogo45@users.noreply.github.com>
2021-10-05 07:53:13 -0300

    JS - Switch

---
+++ js/script.js
@@ -0,0 +1,44 @@
+const cores = "laranja"
+
+
+switch (cores){
+
+
+case "amarelo":
+
+console.log("Amarelo !!")
+
+break;
+
+case "vermelho" :
+
+console.log("Vermelho !!")
+
+break;
+
+case "laranja":
+
+console.log("laranja !!")
+
+break;
+
+
+case "roxo":
+
+console.log("roxo !!")
+
+break;
+
+
+case "verde" :
+
+
+console.log("verde !!")
+
+break;
+
+default:
+
+console.log("Cor inexistente")
+
+}
